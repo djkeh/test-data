1fb4d80d03f3f12d42e01e8a48af5599fea7fce2
Uno Kim <djkehh@gmail.com>
2024-07-20 06:04:42 +0900

    #5 - 스프링 부트 프로젝트 초기화
    인텔리제이에서 Spring Initializr를 통해 프로젝트 초기화하고,
    당장 필요할 것으로 예상되는 디펜던시를 추가함.

---
+++ src/test/java/uno/fastcampus/testdata/TestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package uno.fastcampus.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
