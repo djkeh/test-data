94abb6f2d6b1f5a1cdbc6b97b7e261f7da98081a
Uno Kim <djkehh@gmail.com>
2024-07-30 08:05:45 +0900

    #12 - 엔티티 메타 정보를 관리하는 AuditingField 추가
    모든 엔티티는 관리 목적으로
    도메인 정보와는 직접 연관없는 정보를
    포함할 수 있다.
    예를 들면 생성한 시간, 생성한 사람 등.
    이를 한데 묶어 관리할 수 있도록
    추상 클래스를 설계함.

---
+++ src/main/java/uno/fastcampus/testdata/domain/AuditingFields.java
@@ -0,0 +1,49 @@
+package uno.fastcampus.testdata.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+/**
+ * 각 필요한 테이블에 들어가는 메타정보.
+ * 테이블 데이터가 기록된 시간과 작성자 정보를 담고 있다.
+ * 이 필드들은 Spring Data JPA에 의해 자동으로 관리되므로,
+ * 코드 사용자가 직접 손댈 필요 없게끔 구현되어 있다.
+ *
+ * @author Uno
+ */
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 생성일시
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String modifiedBy; // 수정자
+
+}
