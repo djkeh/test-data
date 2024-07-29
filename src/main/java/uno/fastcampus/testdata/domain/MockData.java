67311de64393d24a7843d1a65b857e16e619d55e
Uno Kim <djkehh@gmail.com>
2024-07-30 03:09:49 +0900

    #9 - 가짜 데이터 도메인 설계
    자동차 이름과 같은 고유명사처럼
    가짜 데이터를 사전식으로 저장해서 관리해야 하는 경우를
    도메인으로 설계함.

---
+++ src/main/java/uno/fastcampus/testdata/domain/MockData.java
@@ -0,0 +1,15 @@
+package uno.fastcampus.testdata.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+
+    private String mockDataType;
+    private String mockDataValue;
+
+}
