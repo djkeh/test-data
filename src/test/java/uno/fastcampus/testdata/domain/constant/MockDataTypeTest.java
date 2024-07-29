a95f57bd15379f2dab5d278fa7adce2c4dd7b368
Uno Kim <djkehh@gmail.com>
2024-07-30 03:01:02 +0900

    #8 - 가짜 데이터 자료형 enum 설계
    가짜 데이터 타입은 DB에 저장 관리하지 않기로 함.
    따라서 타입을 용이하게 관리할 수 있는 enum 방식을 선택.
    enum 정보는 가짜 데이터 생성에 필요한 옵션 정보를 들고 있고,
    이를 프론트에도 넘겨줄 수 있도록 하는 방법을 추가로 고민함.
    구현한 내용을 간단히 문자열 변환하여 테스트함.

---
+++ src/test/java/uno/fastcampus/testdata/domain/constant/MockDataTypeTest.java
@@ -0,0 +1,37 @@
+package uno.fastcampus.testdata.domain.constant;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("[Domain] 테스트 데이터 자료형 테스트")
+class MockDataTypeTest {
+
+    @DisplayName("자료형이 주어지면, 해당 원소의 이름을 리턴한다.")
+    @Test
+    void givenMockDataType_whenReading_thenReturnsEnumElementName() {
+        // given
+        MockDataType mockDataType = MockDataType.STRING;
+
+        // when
+        String elementName = mockDataType.toString();
+
+        // then
+        assertThat(elementName).isEqualTo(MockDataType.STRING.name());
+    }
+
+    @DisplayName("자료형이 주어지면, 해당 원소의 데이터를 리턴한다.")
+    @Test
+    void givenMockDataType_whenReading_thenReturnsEnumElementObject() {
+        // Given
+        MockDataType mockDataType = MockDataType.STRING;
+
+        // When
+        MockDataType.MockDataTypeObject result = mockDataType.toObject();
+
+        // Then
+        assertThat(result.toString()).contains("name", "requiredOptions", "baseType");
+    }
+
+}
