55622227d829acc434ac78b9371e1b04f1057499
Uno Kim <djkehh@gmail.com>
2024-08-01 05:37:40 +0900

    #17 - 테이블 스키마 요청 dto, 스키마 필드 요청 dto를 record에서 class로 전환
    현재 스프링 프레임워크 버전에서는
    컨트롤러 핸들러 메소드의 입력 파라미터로
    `record`를 자유롭게 쓸 수 없음.
    
    이 문제는 아래 참조 링크에 따라 스프링 버전 6.2,
    스프링 부트로는 버전 3.4 이후부터
    수정될 것으로 전망.
    따라서 일단 이 dto들만
    기본 생성자와 setter를 갖춘 class로 전환한다.
    
    추후 프로젝트의 부트 버전을 올리게 되면
    이 부분을 `record`로 되돌리는 것을 검토할 것.
    
    ## Reference
    
    * https://github.com/spring-projects/spring-framework/issues/32426

--- src/main/java/uno/fastcampus/testdata/dto/request/TableSchemaRequest.java
+++ src/main/java/uno/fastcampus/testdata/dto/request/TableSchemaRequest.java
@@ -1,24 +1,25 @@
 package uno.fastcampus.testdata.dto.request;
 
+import lombok.*;
 import uno.fastcampus.testdata.dto.TableSchemaDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record TableSchemaRequest(
-        String schemaName,
-        String userId,
-        List<SchemaFieldRequest> schemaFields
-) {
+// TODO: 스프링 부트 3.4를 쓸 수 있게 되면, record로 되돌리는 것을 검토하기
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@Data
+public class TableSchemaRequest {
 
-    public static TableSchemaRequest of(String schemaName, String userId, List<SchemaFieldRequest> schemaFields) {
-        return new TableSchemaRequest(schemaName, userId, schemaFields);
-    }
+    private String schemaName;
+    private String userId;
+    private List<SchemaFieldRequest> schemaFields;
 
     public TableSchemaDto toDto() {
         return TableSchemaDto.of(
-                schemaName(),
-                userId(),
+                schemaName,
+                userId,
                 null,
                 schemaFields.stream()
                         .map(SchemaFieldRequest::toDto)
