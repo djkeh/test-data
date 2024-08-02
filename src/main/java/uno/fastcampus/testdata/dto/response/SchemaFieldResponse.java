8d70d1df33387e27f7c7c23077ec1c23be50c69a
Uno Kim <djkehh@gmail.com>
2024-08-02 16:00:50 +0000

    #56 - [수정 제안] 스키마 필드 응답 dto의 컬럼 순서 조정 리팩토링이라면 이것도 같이 해주시면 좋을 듯요. 필드명이 타입보다 앞에 있는 것이 자연스러울 것 같습니다.
    https://github.com/djkeh/test-data/pull/57

--- src/main/java/uno/fastcampus/testdata/dto/response/SchemaFieldResponse.java
+++ src/main/java/uno/fastcampus/testdata/dto/response/SchemaFieldResponse.java
@@ -4,8 +4,8 @@ import uno.fastcampus.testdata.domain.constant.MockDataType;
 import uno.fastcampus.testdata.dto.SchemaFieldDto;
 
 public record SchemaFieldResponse(
-        MockDataType mockDataType,
         String fieldName,
+        MockDataType mockDataType,
         Integer fieldOrder,
         Integer blankPercent,
         String typeOptionJson,
@@ -14,8 +14,8 @@ public record SchemaFieldResponse(
 
     public static SchemaFieldResponse fromDto(SchemaFieldDto dto) {
         return new SchemaFieldResponse(
-                dto.mockDataType(),
                 dto.fieldName(),
+                dto.mockDataType(),
                 dto.fieldOrder(),
                 dto.blankPercent(),
                 dto.typeOptionJson(),
