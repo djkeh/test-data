eb1f25c9a7f55d3ee5b985f8362428144a3bcda1
Mikonu <djkehh@naver.com>
2024-08-04 03:24:31 +0900

    #56 - 테이블 스키마 컨트롤러 테스트 제목 리팩토링
    `페이지 -> 뷰` 스타일의 제목은
    요청 응답을 연상시켜 컨트롤러 테스트의
    입출력 흐름을 보여주는 괜찮은 스타일인 것 같지만
    다시 생각해보면 그냥 같은 내용 반복임.
    별로 이 스타일이 추가로 전달하는 정보가 없음.
    이에 스타일을 바꾼다.
    차라리 입력이나 사용자가 이용하는 상황을 묘사하는
    부가 정보를 나중에 더 넣는 것이 나을 듯.

--- src/test/java/uno/fastcampus/testdata/controller/TableSchemaControllerTest.java
+++ src/test/java/uno/fastcampus/testdata/controller/TableSchemaControllerTest.java
@@ -29,7 +29,7 @@ record TableSchemaControllerTest(
         @Autowired FormDataEncoder formDataEncoder
 ) {
 
-    @DisplayName("[GET] 테이블 스키마 페이지 -> 테이블 스키마 뷰 (정상)")
+    @DisplayName("[GET] 테이블 스키마 조회, 비로그인 최초 진입 (정상)")
     @Test
     void givenNothing_whenRequesting_thenShowsTableSchemaView() throws Exception {
         // Given
@@ -67,7 +67,7 @@ record TableSchemaControllerTest(
                 .andExpect(redirectedUrl("/table-schema"));
     }
 
-    @DisplayName("[GET] 내 스키마 목록 페이지 -> 내 스키마 목록 뷰 (정상)")
+    @DisplayName("[GET] 내 스키마 목록 조회 (정상)")
     @Test
     void givenAuthenticatedUser_whenRequesting_thenShowsMySchemaView() throws Exception {
         // Given
@@ -94,7 +94,7 @@ record TableSchemaControllerTest(
                 .andExpect(redirectedUrl("/my-schemas"));
     }
 
-    @DisplayName("[GET] 테이블 스키마 파일 다운로드 -> 테이블 스키마 파일 (정상)")
+    @DisplayName("[GET] 테이블 스키마 파일 다운로드 (정상)")
     @Test
     void givenTableSchema_whenDownloading_thenReturnsFile() throws Exception {
         // Given
