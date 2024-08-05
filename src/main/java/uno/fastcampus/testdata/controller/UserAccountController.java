53950423c6bb35400614534c4e83999b609b86a5
Uno Kim <djkehh@gmail.com>
2024-08-05 11:13:41 +0900

    #58 - 내 정보 뷰 구현
    마크업 템플릿은 chatgpt로 골격을 잡고 세부 수정하여 구현.
    회원 컨트롤러에서 데모 관찰 목적으로 가짜 회원 정보 추가하고
    테스트에 반영.

--- src/main/java/uno/fastcampus/testdata/controller/UserAccountController.java
+++ src/main/java/uno/fastcampus/testdata/controller/UserAccountController.java
@@ -1,13 +1,17 @@
 package uno.fastcampus.testdata.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserAccountController {
 
     @GetMapping("/my-account")
-    public String myAccount() {
+    public String myAccount(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        model.addAttribute("email", "홍길동@email.com");
+
         return "my-account";
     }
 
