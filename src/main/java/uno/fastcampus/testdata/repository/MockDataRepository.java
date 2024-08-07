608a48271e9d782a2661217a2f099cd33c2cd385
Uno Kim <djkehh@gmail.com>
2024-08-08 04:36:28 +0900

    #30 - 잦은 select 쿼리 발생이 우려되는 곳에 캐시 적용
    부트에서 제공하는 Spring Cache Abstraction을 이용해
    간단하게 애노테이션 방식으로 캐시를 적용.
    캐시는 `ConcurrentMap`으로
    애플리케이션이 동작하는 동안 내부 메모리에 구현됨.
    인테그레이션 테스트를 동작시키면 쿼리 발생이
    1개로 줄어드는 것을 관찰 가능.

--- src/main/java/uno/fastcampus/testdata/repository/MockDataRepository.java
+++ src/main/java/uno/fastcampus/testdata/repository/MockDataRepository.java
@@ -1,5 +1,6 @@
 package uno.fastcampus.testdata.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uno.fastcampus.testdata.domain.MockData;
 import uno.fastcampus.testdata.domain.constant.MockDataType;
@@ -7,5 +8,6 @@ import uno.fastcampus.testdata.domain.constant.MockDataType;
 import java.util.List;
 
 public interface MockDataRepository extends JpaRepository<MockData, Long> {
+    @Cacheable("mockData")
     List<MockData> findByMockDataType(MockDataType mockDataType);
 }
