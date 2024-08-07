70193ae5a5d1e36563078ede42bfb26102e5b13c
Uno Kim <djkehh@gmail.com>
2024-08-07 09:54:10 +0900

    #22 - 깃헙 OAuth 2.0 인증 기술 준비
    스프링 부트 서비스에서 깃헙 인증을 받아들일 수 있도록
    관련 설정을 추가.
    인증 정보는 우리 서비스에 맞는 깃헙 인증 정보로
    편리하게 쓸 수 있도록, `OAuth2User` 인터페이스를
    `GithubUser`로 구현함.
    데이터를 매핑하는 방법과 참고 문서는 소스코드 참고.
    깃헙 연결에 사용할 애플리케이션 프로퍼티는
    보안에 민감한 정보이므로, 환경변수로 따로 빼서 작성함.

--- src/main/java/uno/fastcampus/testdata/config/SecurityConfig.java
+++ src/main/java/uno/fastcampus/testdata/config/SecurityConfig.java
@@ -3,7 +3,12 @@ package uno.fastcampus.testdata.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import uno.fastcampus.testdata.dto.security.GithubUser;
 
 @Configuration
 public class SecurityConfig {
@@ -14,6 +19,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> GithubUser.from(delegate.loadUser(userRequest).getAttributes());
     }
 
 }
