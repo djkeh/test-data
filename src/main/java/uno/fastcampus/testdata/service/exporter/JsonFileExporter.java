f30b5f25c79d2c1cd943352d4cc3d10788af5c99
Mikonu <djkehh@naver.com>
2024-08-20 19:22:36 +0900

    #97 - Json 파일 출력기 구현
    `.json` 형식으로 파일을 출력해주는 기능을 추가한다.

---
+++ src/main/java/uno/fastcampus/testdata/service/exporter/JsonFileExporter.java
@@ -0,0 +1,57 @@
+package uno.fastcampus.testdata.service.exporter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uno.fastcampus.testdata.domain.constant.ExportFileType;
+import uno.fastcampus.testdata.dto.SchemaFieldDto;
+import uno.fastcampus.testdata.dto.TableSchemaDto;
+import uno.fastcampus.testdata.service.generator.MockDataGeneratorContext;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JsonFileExporter implements MockDataFileExporter {
+
+    private final MockDataGeneratorContext mockDataGeneratorContext;
+    private final ObjectMapper mapper;
+
+    @Override
+    public ExportFileType getType() {
+        return ExportFileType.JSON;
+    }
+
+    @Override
+    public String export(TableSchemaDto dto, Integer rowCount) {
+        try {
+            List<Map<String, Object>> list = new ArrayList<>();
+
+            IntStream.range(0, rowCount).forEach(i -> {
+                Map<String, Object> map = new LinkedHashMap<>();
+                dto.schemaFields().stream()
+                        .sorted(Comparator.comparing(SchemaFieldDto::fieldOrder))
+                        .forEach(field -> {
+                            map.put(field.fieldName(), mockDataGeneratorContext.generate(
+                                    field.mockDataType(),
+                                    field.blankPercent(),
+                                    field.typeOptionJson(),
+                                    field.forceValue()
+                            ));
+                        });
+                list.add(map);
+            });
+
+            return mapper.writeValueAsString(list);
+        } catch (JsonProcessingException e) {
+            log.warn("테이블 스키마 데이터를 JSON으로 변환하는데 실패했습니다 - {}", dto, e);
+            return "";
+        }
+    }
+
+}
